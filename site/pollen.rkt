10c06f09c7945d2d74d68a998496060343f42619
sloane <git@sloanelybutsurely.com>
2024-11-09 11:14:44 -0500

    initial layout of new site

--- site/pollen.rkt
+++ site/pollen.rkt
@@ -2,16 +2,20 @@
 (require pollen/decode pollen/misc/tutorial txexpr)
 (provide (all-defined-out))
 
+#| site globals |#
 (define site-name "sloane.sh")
 (define email "sloane@fastmail.com")
 
+#| custom elements |#
+
+#| plain text decoding |#
 (define txexpr-elements-proc decode-paragraphs)
 (define string-proc (compose1 smart-quotes smart-dashes))
-
 (define (root . elements)
    (txexpr 'root empty (decode-elements elements
      #:txexpr-elements-proc txexpr-elements-proc
      #:string-proc string-proc)))
 
+#| setup |#
 (module setup racket/base
   (provide (all-defined-out)))
